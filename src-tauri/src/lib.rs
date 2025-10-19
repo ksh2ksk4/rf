c80de2dc120215503ad4ae411426d4b2d1c8425a
ksh2ksk4 <github.clicker425@passinbox.com>
2025-10-19 15:23:03 +0900

    Update to show file list

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,35 @@
+use serde::Serialize;
+use std::fs;
+
+#[derive(Debug, Serialize)]
+struct FileInfo {
+    name: String,
+    path: String,
+    size: u64,
+}
+
+#[tauri::command]
+fn read_dir(path: String) -> Result<Vec<FileInfo>, String> {
+    let mut entries = Vec::<FileInfo>::new();
+
+    for d in fs::read_dir(&path).map_err(|e| e.to_string())? {
+        let de = d.map_err(|e| e.to_string())?;
+        let metadata = de.metadata().map_err(|e| e.to_string())?;
+        entries.push(FileInfo {
+            name: de.file_name().to_string_lossy().to_string(),
+            path: de.path().to_string_lossy().to_string(),
+            size: metadata.len(),
+        });
+    }
+
+    Ok(entries)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![read_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,10 @@
+use gloo_utils::format::JsValueSerdeExt;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
+
+const INIT_DIR: &str = "/Users/ksh2ksk4/Downloads";
 
 #[wasm_bindgen]
 extern "C" {
@@ -9,10 +12,53 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+struct FileInfo {
+    name: String,
+    path: String,
+    size: u64,
+}
+
 #[function_component(App)]
 pub fn app() -> Html {
+    let files = use_state(|| Vec::<FileInfo>::new());
+
+    // 初回マウント時に実行されるフック
+    {
+        let files = files.clone();
+        use_effect_with((), move |_| {
+            spawn_local(async move {
+                let path = INIT_DIR;
+                let args = JsValue::from_serde(&serde_json::json!({"path": path})).unwrap();
+                files.set(invoke("read_dir", args).await.into_serde().unwrap());
+            });
+
+            || {}
+        });
+    }
+
     html! {
         <main class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>{"name"}</th>
+                        <th>{"size"}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {for files.iter().map(|f| {
+                        let name = f.name.clone();
+
+                        html! {
+                            <tr>
+                                <td>{name}</td>
+                                <td>{f.size}</td>
+                            </tr>
+                        }
+                    })}
+                </tbody>
+            </table>
         </main>
     }
 }
