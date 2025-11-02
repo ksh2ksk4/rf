7de5dde060617eb0a4ca1f04409c1d2e557cee8d
ksh2ksk4 <github.clicker425@passinbox.com>
2025-11-03 03:05:25 +0900

    Update to show error messages at toast

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use gloo_timers::future::TimeoutFuture;
 use gloo_utils::format::JsValueSerdeExt;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
@@ -5,6 +6,7 @@ use wasm_bindgen_futures::spawn_local;
 use web_sys::console;
 use yew::prelude::*;
 
+const TOAST_DURATION: u32 = 5000;
 const INIT_PATH: &str = "/Users/ksh2ksk4/Downloads";
 const UNITS: [&str; 5] = ["B", "KiB", "MiB", "GiB", "TiB"];
 
@@ -105,15 +107,52 @@ impl NavigationHistory {
     }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum ToastKind {
+    Success,
+    Info,
+    Warning,
+    Error,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Toast {
+    id: usize,
+    kind: ToastKind,
+    message: String,
+}
+
 #[function_component(App)]
 pub fn app() -> Html {
     let navigation_history = use_state(|| NavigationHistory::new());
     let files = use_state(|| Vec::<FileInfo>::new());
+    let toasts = use_state(|| Vec::<Toast>::new());
+    let next_toast_id = use_state(|| 1_usize);
+    let push_toast = {
+        let toasts = toasts.clone();
+        let next_toast_id = next_toast_id.clone();
+        Callback::from(move |(kind, message): (ToastKind, String)| {
+            let mut v = (*toasts).clone();
+            let id = *next_toast_id;
+            v.push(Toast { id, kind, message });
+            toasts.set(v);
+            next_toast_id.set(id + 1);
+
+            let toasts = toasts.clone();
+            spawn_local(async move {
+                TimeoutFuture::new(TOAST_DURATION).await;
+                let mut new_value = (*toasts).clone();
+                new_value.retain(|v| v.id != id);
+                toasts.set(new_value);
+            });
+        })
+    };
 
     // 初回マウント時に実行されるフック
     {
         let navigation_history = navigation_history.clone();
         let files = files.clone();
+        let push_toast = push_toast.clone();
         use_effect_with((), move |_| {
             spawn_local(async move {
                 let path = navigation_history.paths[0].clone();
@@ -121,7 +160,10 @@ pub fn app() -> Html {
                 invoke_r("read_dir", args)
                     .await
                     .map(|v| files.set(v.into_serde().unwrap()))
-                    .unwrap_or_else(|e| console::error_1(&e));
+                    .unwrap_or_else(|e| {
+                        console::error_1(&e);
+                        push_toast.emit((ToastKind::Error, format!("{e:?}")));
+                    });
             });
 
             || {}
@@ -148,17 +190,22 @@ pub fn app() -> Html {
     let handle_back_click = {
         let navigation_history = navigation_history.clone();
         let files = files.clone();
+        let push_toast = push_toast.clone();
         Callback::from(move |_| {
             let mut nh = (*navigation_history).clone();
             let path = nh.back().unwrap_or(INIT_PATH.to_string());
             navigation_history.set(nh);
             let files = files.clone();
+            let push_toast = push_toast.clone();
             spawn_local(async move {
                 let args = JsValue::from_serde(&serde_json::json!({"path": path})).unwrap();
                 invoke_r("read_dir", args)
                     .await
                     .map(|v| files.set(v.into_serde().unwrap()))
-                    .unwrap_or_else(|e| console::error_1(&e));
+                    .unwrap_or_else(|e| {
+                        console::error_1(&e);
+                        push_toast.emit((ToastKind::Error, format!("{e:?}")));
+                    });
             });
         })
     };
@@ -167,17 +214,22 @@ pub fn app() -> Html {
     let handle_forward_click = {
         let navigation_history = navigation_history.clone();
         let files = files.clone();
+        let push_toast = push_toast.clone();
         Callback::from(move |_| {
             let mut nh = (*navigation_history).clone();
             let path = nh.forward().unwrap_or(nh.current().to_string());
             navigation_history.set(nh);
             let files = files.clone();
+            let push_toast = push_toast.clone();
             spawn_local(async move {
                 let args = JsValue::from_serde(&serde_json::json!({"path": path})).unwrap();
                 invoke_r("read_dir", args)
                     .await
                     .map(|v| files.set(v.into_serde().unwrap()))
-                    .unwrap_or_else(|e| console::error_1(&e));
+                    .unwrap_or_else(|e| {
+                        console::error_1(&e);
+                        push_toast.emit((ToastKind::Error, format!("{e:?}")));
+                    });
             });
         })
     };
@@ -186,16 +238,24 @@ pub fn app() -> Html {
     let handle_select_dir_click = {
         let navigation_history = navigation_history.clone();
         let files = files.clone();
+        let push_toast = push_toast.clone();
         Callback::from(move |_| {
             let navigation_history = navigation_history.clone();
             let files = files.clone();
+            let push_toast = push_toast.clone();
             spawn_local(async move {
                 let path = invoke_no_args("select_dir").await.as_string().unwrap();
                 let args = JsValue::from_serde(&serde_json::json!({"path": path})).unwrap();
                 invoke_r("read_dir", args)
                     .await
-                    .map(|v| files.set(v.into_serde().unwrap()))
-                    .unwrap_or_else(|e| console::error_1(&e));
+                    .map(|v| {
+                        files.set(v.into_serde().unwrap());
+                        push_toast.emit((ToastKind::Success, "read_dir() succeeded".to_string()));
+                    })
+                    .unwrap_or_else(|e| {
+                        console::error_1(&e);
+                        push_toast.emit((ToastKind::Error, format!("{e:?}")));
+                    });
                 let mut nh = (*navigation_history).clone();
                 nh.push(&path);
                 navigation_history.set(nh);
@@ -205,6 +265,51 @@ pub fn app() -> Html {
 
     html! {
         <div class="min-h-screen min-w-screen flex flex-col">
+            <div class="toast-area">
+                {for toasts.iter().map(|t| {
+                    let toasts = toasts.clone();
+                    let id = t.id;
+                    let handle_close_click = Callback::from(move |_| {
+                        let mut new_value = (*toasts).clone();
+                        new_value.retain(|v| v.id != id);
+                        toasts.set(new_value);
+                    });
+                    html! {
+                        <div class={classes!(
+                            "toast-base",
+                            match t.kind {
+                                ToastKind::Success => "toast-success",
+                                ToastKind::Info => "toast-info",
+                                ToastKind::Warning => "toast-warning",
+                                ToastKind::Error => "toast-error",
+                            }
+                        )}>
+                            <i
+                                class={classes!(
+                                    "mr-2",
+                                    "select-none",
+                                    "nf",
+                                    match t.kind {
+                                        ToastKind::Success => "nf-fa-ok_sign",
+                                        ToastKind::Info => "nf-fa-circle_info",
+                                        ToastKind::Warning => "nf-fa-warning",
+                                        ToastKind::Error => "nf-fa-triangle_exclamation"
+                                    }
+                                )}
+                                aria-hidden="true"
+                            />
+                            <span class="flex-1">{t.message.clone()}</span>
+                            <button
+                                class="ml-3 opacity-80 hover:opacity-100"
+                                onclick={handle_close_click}
+                                aria-label="dismiss"
+                            >
+                                <i class="nf nf-fa-window_close" />
+                            </button>
+                        </div>
+                    }
+                })}
+            </div>
             <header>
                 <div class="toolbar">
                     <button
@@ -285,11 +390,13 @@ pub fn app() -> Html {
                                 let handle_dir_click = {
                                     let navigation_history = navigation_history.clone();
                                     let files = files.clone();
+                                    let push_toast = push_toast.clone();
                                     let path = f.path.clone();
                                     Callback::from(move |e: MouseEvent| {
                                         e.prevent_default();
 
                                         if !is_dir {
+                                            let push_toast = push_toast.clone();
                                             let path = path.clone();
                                             spawn_local(async move {
                                                 let args = JsValue::from_serde(
@@ -298,7 +405,10 @@ pub fn app() -> Html {
                                                 invoke_r("open_file", args)
                                                     .await
                                                     .map(|_| console::info_1(&"open_file() succeeded".into()))
-                                                    .unwrap_or_else(|e| console::error_1(&e));
+                                                    .unwrap_or_else(|e| {
+                                                        console::error_1(&e);
+                                                        push_toast.emit((ToastKind::Error, format!("{e:?}")));
+                                                    });
                                             });
 
                                             return;
@@ -309,6 +419,7 @@ pub fn app() -> Html {
                                         navigation_history.set(nh);
 
                                         let files = files.clone();
+                                        let push_toast = push_toast.clone();
                                         let path = path.clone();
                                         spawn_local(async move {
                                             let args = JsValue::from_serde(
@@ -317,7 +428,10 @@ pub fn app() -> Html {
                                             invoke_r("read_dir", args)
                                                 .await
                                                 .map(|v| files.set(v.into_serde().unwrap()))
-                                                .unwrap_or_else(|e| console::error_1(&e));
+                                                .unwrap_or_else(|e| {
+                                                    console::error_1(&e);
+                                                    push_toast.emit((ToastKind::Error, format!("{e:?}")));
+                                                });
                                         });
                                     })
                                 };
